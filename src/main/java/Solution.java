d9398f5f4b205a8f017b25ef87c71e5f5cc20c18
Dmitriy Goldobin <dgoldobin@odesk.com>
2017-12-08 09:22:50 +0500

    xx

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -8,8 +8,10 @@ public class Solution {
     private static Integer MAX_CHARS = 10;
 
     public static void main(String[] args) {
-        char[] str = new char[50000];
-        Arrays.fill(str, 'a');
+        sc.nextInt();
+        int cases = sc.nextInt();
+
+        char[] str = sc.next().toCharArray();
 
         List<Long> times = new ArrayList<>();
         times.add(currentTimeMillis());
@@ -18,7 +20,7 @@ public class Solution {
         int[] prevCharPos = new int[MAX_CHARS];
         for (int i = 0; i < str.length; ++i) {
             int c = str[i] - 'a';
-            dist[i] = i - prevCharPos[c] + 1;
+            dist[i] = i - prevCharPos[c];
             prevCharPos[c] = i + 1;
         }
         times.add(currentTimeMillis());
@@ -32,43 +34,84 @@ public class Solution {
                 if (diff != 0)
                     return diff;
             }
-            return a - b;
+            return b - a;
         });
         times.add(currentTimeMillis());
         int[][] lcp = lcp(sa, dist);
 
+        StringBuilder sb = new StringBuilder();
+        while (cases-- > 0) {
+            int pos = sc.nextInt() - 1;
+            int length = sc.nextInt();
+            sb.append(getLcpCount(sa, lcp, pos, length));
+            sb.append("\n");
+        }
         times.add(currentTimeMillis());
 
-        for (int i = 0; i < times.size() - 1; ++i)
-            out.printf("%.3f sec\n", (times.get(i + 1) - times.get(i)) / 1000.0);
+        out.println(sb.toString());
+
+//        for (int i = 0; i < times.size() - 1; ++i)
+//            out.printf("%.3f sec\n", (times.get(i + 1) - times.get(i)) / 1000.0);
     }
 
-    private static int[][] lcp(Integer[] sa, int[] str) {
-        int[] revSa = new int[sa.length], log = new int[sa.length], lcp1 = new int[sa.length-1];
-        for (int i = 0; i < revSa.length; ++i) {
-            revSa[sa[i]] = i;
-            log[i] = log[i / 2] + 1;
+    private static int getLcpCount(Integer[] sa, int[][] lcp, int from, int length) {
+        int saFrom = lcp[0][from];
+        int min = 0, max = saFrom;
+        while (min < max) {
+            int cur = (min + max) / 2;
+            if (getLcp(lcp, from, sa[cur]) < length)
+                min = cur + 1;
+            else
+                max = cur;
         }
-        for (int i = 0, curLcp = 0; i < str.length; ++i) {
-            int sa = revSa[i];
-            if (sa < lcp1.length) {
-
-            }
+        int first = max;
+        min = saFrom;
+        max = sa.length;
+        while (min < max) {
+            int cur = (min + max) / 2;
+            if (getLcp(lcp, from, sa[cur]) < length)
+                max = cur;
+            else
+                min = cur + 1;
         }
-
-
-        int[][] lcp = new int[log[log.length - 1] + 1][];
-
-        return lcp;
+        return max - first;
     }
 
-    private static int getLco(int[][] lcp, int from, int to) {
+    private static int getLcp(int[][] lcp, int from, int to) {
         if (from == to)
             return lcp[0].length - from;
         int sa1 = min(lcp[0][from], lcp[0][to]);
         int sa2 = max(lcp[0][from], lcp[0][to]);
         int log = lcp[1][sa2 - sa1];
-        return min(lcp[log][sa1], lcp[log][sa2 - 1 << (log - 2)]);
+        return min(lcp[log][sa1], lcp[log][sa2 - (1 << (log - 2))]);
+    }
 
+    private static int[][] lcp(Integer[] sa, int[] str) {
+        int[] revSa = new int[sa.length], log = new int[sa.length], lcp1 = new int[sa.length - 1];
+        for (int i = 0; i < revSa.length; ++i) {
+            revSa[sa[i]] = i;
+            log[i] = log[i / 2] + 1;
+        }
+        for (int i = 0, curLcp = 0; i < str.length; ++i) {
+            int saPtr = revSa[i];
+            if (saPtr != lcp1.length) {
+                for (int j = sa[saPtr + 1], top = str.length - max(i, j); curLcp < top; ++curLcp)
+                    if (min(curLcp, str[i + curLcp]) != min(curLcp, str[j + curLcp]))
+                        break;
+                lcp1[saPtr] = curLcp;
+            }
+            if (curLcp > 0)
+                --curLcp;
+        }
+        int[][] lcp = new int[log[log.length - 1] + 1][];
+        lcp[0] = revSa;
+        lcp[1] = log;
+        lcp[2] = lcp1;
+        for (int i = 3; i < lcp.length; ++i) {
+            lcp[i] = new int[lcp[i-1].length - (1 << (i - 3))];
+            for (int j = 0; j < lcp[i].length; ++j)
+                lcp[i][j] = min(lcp[i-1][j], lcp[i-1][j + (1 << (i - 3))]);
+        }
+        return lcp;
     }
 }
